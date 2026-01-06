490fbbe026ffb5ed7506624aba522b733eb6d352
Roberto Torres <robert.torres.lopez@gmail.com>
2026-01-06 15:26:31 -0600

    contract functionality (#27)

--- test/factories/contracts/contract.factory.ts
+++ test/factories/contracts/contract.factory.ts
@@ -5,8 +5,6 @@ import type { DataSource } from 'typeorm';
 
 import { Contract } from '../../../src/contracts/entities/contract.entity';
 import { CONTRACT_STATUS } from '../../../src/contracts/types/contract-status.types';
-import { BrandFactory } from '../brands/brands.factories';
-import type { Brand } from '../../../src/brands/entities/brand.entity';
 
 export class ContractFactory extends Factory<Contract> {
   protected entity = Contract;
@@ -19,32 +17,14 @@ export class ContractFactory extends Factory<Contract> {
 
   protected attrs(): FactorizedAttrs<Contract> {
     return {
-      brandId: 0,
-      clientName: faker.person.fullName(),
-      clientPhone: faker.phone.number(),
-      clientEmail: faker.internet.email(),
-      notes: faker.lorem.sentence(),
       status: CONTRACT_STATUS.PENDING,
       totalAmount: 0,
-      tokenId: null,
+      token: faker.string.alphanumeric(100),
     };
   }
 
-  async createForBrand(brand: Brand, attrs?: Partial<Contract>): Promise<Contract> {
-    const contract = await this.make({ brandId: brand.id, ...attrs });
-    return this.dataSource.getRepository(Contract).save(contract);
-  }
-
   async create(attrs?: Partial<Contract>): Promise<Contract> {
-    let brandId = attrs?.brandId;
-    if (!brandId) {
-      const brandFactory = new BrandFactory(this.dataSource);
-      const brand = await brandFactory.create();
-      brandId = brand.id;
-    }
-    const contract = await this.make({ brandId, ...attrs });
+    const contract = await this.make({ ...attrs });
     return this.dataSource.getRepository(Contract).save(contract);
   }
 }
-
-
